113b67199b247c86f966ea04f152a3b424887b04
Eleilton Santos <ilton.sa@outlook.com>
2025-07-03 16:44:07 -0300

    Adicionando doc em Java

---
+++ Aulas/Logica de programacao.java
@@ -0,0 +1,50 @@
+
+/* Faça um programa que calcule e imprima o salário a ser transferido para um funcionário
+  
+Para realizar o calculo receba o valor bruto do salário e o adicional dos beneficios.
+
+O salário a ser trasnferido é calculado da seguinte maneira:
+    
+valor bruto do salario - percentual de imposto mediana ai salário + adicional dos beneficios
+    
+Para calcular o percentual de imposto segue as aliquotas:
+    
+De R$ 0.00 a R$ 1.100,00 = 5.00%
+DE R$ 1.100,00 a R$ 2.500,00 = 10.00 %
+Maior que 2.500,00 = 15.00%
+*/
+
+// Para ler e escrever dados em Java:
+// new Scanner(System.in): Cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// System.out.print: Imprime um texto de saída (output) e pulando uma linha
+
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+    // Ler os valores de entrada
+    Scanner leitorDeEntradas = new Scanner(System.in);
+    float valorSalario = leitorDeEntradas.nextFloat();
+    float valorBeneficio = leitorDeEntradas.nextFloat();
+
+    //Atribui a aliquota de 5% mediante o salario:
+    float valorImposto = 0;
+    if (valorSalario >= 0 && valorSalario <= 1100) {
+        valorImposto = 0.05F * valorSalario;
+    }
+    //Atribui a aliquota de 10% mediante o salario:
+    else if (valorSalario >= 1100.01 && valorSalario <= 2500) {
+        valorImposto = 0.10F * valorSalario;
+    }
+    //Atribui a aliquota de 15% mediante o salario
+    else (valorSalario >= 2500) {
+        valorImposto = 0.15F * valorSalario;
+    }
+   
+    // Calcula e imprime a Saída (Com 2 casas decimais):
+    float saida = valorSalario - valorImposto + valorBeneficio;
+    System.out.print(String.format("%.2f", saida));
+    }
+}
+
