1dc271b9b8f20581b47b7b073a5cc2d153de67f8
Eleilton Santos <ilton.sa@outlook.com>
2025-07-04 11:25:35 -0300

    Enviando teste de lógica

--- Aulas/desconto.py
+++ Aulas/desconto.py
@@ -1,4 +1,4 @@
-descontos = {
+"""descontos = {
     "DESCONTO10": 0.10,
     "DESCONTO20": 0.20,
     "SEM_DESCONTO": 0.00
@@ -12,5 +12,28 @@ if cupom in descontos:
     preco_final = preco * (1 - desconto)
     print(f"{preco_final:.2f}")
 else:
-    print("Cupom inválido")
+    print("Cupom inválido")"""
 
+
+
+descontos = {
+    "DESCONTO10": 0.10,
+    "DESCONTO20": 0.20,
+    "SEM_DESCONTO": 0.00
+}
+
+preco = float(input())
+cupom = input().strip().upper()
+
+if preco == 100:
+    desconto = descontos["DESCONTO10"]
+    
+elif preco == 200:
+    desconto = descontos["DESCONTO20"]
+  
+elif preco == 50:
+    desconto = descontos["SEM_DESCONTO"]
+   
+valor_desconto = preco * desconto
+preco_final = preco - valor_desconto
+print(preco_final)
